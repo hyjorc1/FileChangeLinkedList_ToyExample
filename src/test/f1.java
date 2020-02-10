17cbbed606016241e2a16c3de4543ab9d3e00c8c
yijiahuang <hyj@iastate.edu>
2020-02-10 15:35:16 -0600

    c10

--- src/test/f1.java
+++ src/test/f1.java
@@ -4,5 +4,6 @@ public class f1 {
 	public static void main(String[] args) {
 		int a = 0;
 		int i = 0;
+		int c10 = 0;
 	}
 }
--- src/test/f2.java
+++ src/test/f2.java
@@ -4,7 +4,7 @@ public class f2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int c10 = 0;
 	}
 
 }
